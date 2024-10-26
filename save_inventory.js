95fae5aa3fbf5cc74cda3c05c9892d536eeea793
Jaan Jahilo <jaanjahilo@outlook.com>
2024-10-26 09:04:21 +0300

    Init tampermonkey script

---
+++ save_inventory.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         MWI - Save Inventory
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Get MWI Inventory and save as JSON
+// @author       JaanJah
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=milkywayidle.com
+// @match        https://www.milkywayidle.com/game?characterId=*
+// @match        https://test.milkywayidle.com/game?characterId=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
