53313d18e5b3308aa354917ea1bfe61ed11885d9
Jaan Jahilo <jaanjahilo@outlook.com>
2024-10-26 11:40:24 +0300

    Add readInventories to read in input directory content

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	inventories := readInventories("input")
+	fmt.Println(len(inventories))
+}
+
+func readInventories(directory string) []string {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+
+	var inventories []string
+
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".json" {
+			filePath := filepath.Join(directory, file.Name())
+			data, err := os.ReadFile(filePath)
+			if err != nil {
+				log.Printf("Failed to read file %s: %v", filePath, err)
+				continue
+			}
+			inventories = append(inventories, string(data))
+		}
+	}
+
+	return inventories
 }
